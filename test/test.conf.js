efc8212aa77c681ffb30cd18c0416b6697c34382
Shaun Grady <sgrady@orcasnet.com>
2014-01-17 09:40:37 -0800

    Begin adding tests
    Only the service at this point

---
+++ test/test.conf.js
@@ -0,0 +1,27 @@
+module.exports = function (config) {
+  config.set({
+    basePath : '..',
+    files : [
+        'bower_components/angular/angular.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+        'bower_components/momentjs/moment.js',
+        'src/angular-moment.js',
+        'test/*.spec.js'
+    ],
+
+    singleRun : true,
+
+    browsers : ['Chrome', 'Firefox'],
+
+    frameworks : ["jasmine"],
+
+    preprocessors : {
+        '**/src/*.js': 'coverage'
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit'
+    reporters : ['progress', 'coverage']
+
+  });
+};
