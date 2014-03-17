2e81fc22a6db7524fcf5f9b66511e39c8838b984
Shaun Grady <sgrady@orcasnet.com>
2014-03-17 16:50:04 -0700

    Tests to picker!

--- test/directives.picker.spec.js
+++ test/directives.picker.spec.js
@@ -4,6 +4,7 @@ describe('momentPicker directive', function () {
 
     var $moment, $scope, $compile, compileHtml;
 
+
     beforeEach(module('moment'));
     beforeEach(module('templates'));
 
@@ -32,12 +33,165 @@ describe('momentPicker directive', function () {
     }));
 
     describe('controller initialization', function() {
-        it('should set display to the current date', function() {
+      
+        it('should set displayMoment to the current date', function() {
+          var picker   = compileHtml('<div moment-picker="date"></div>'),
+              isoScope = picker.isolateScope(),
+              ctrl     = picker.controller('momentPicker');
+          expect(ctrl.displayMoment.isSame($moment(), 'hour')).toBeTruthy();
+        });
+
+        it('should set visible to true', function() {
+          var picker = compileHtml('<div moment-picker="date"></div>'),
+              ctrl   = picker.controller('momentPicker');
+          expect(ctrl.visible).toBeTruthy();
+          expect(ctrl.hidden).toBeFalsy();
+        });
+
+        it('should set pickedMoment to model date', function() {
+          $scope.$apply(function() {
+            $scope.date = $moment('01/01/2000').format($moment.$defaultModelDate);
+          });
+
           var picker = compileHtml('<div moment-picker="date"></div>'),
               ctrl   = picker.controller('momentPicker');
 
-          expect(ctrl.displayMoment.isSame($moment(), 'hour')).toBeTruthy();
+          expect(ctrl.pickedMoment.isSame('01/01/2000')).toBeTruthy();
         });
+
+        it('should not set pickedMoment to invalid model date', function() {
+          $scope.$apply(function() {
+            $scope.date = '*<(8^D)';
+          });
+
+          var picker = compileHtml('<div moment-picker="date"></div>'),
+              ctrl   = picker.controller('momentPicker');
+
+          expect(ctrl.pickedMoment).toBeFalsy();
+        });
+
+        it('should set displayMoment to match pickedMoment', function() {
+          $scope.$apply(function() {
+            $scope.date = $moment('01/01/2000').format($moment.$defaultModelDate);
+          });
+
+          var picker = compileHtml('<div moment-picker="date"></div>'),
+              ctrl   = picker.controller('momentPicker');
+
+          expect(ctrl.displayMoment.isSame('01/01/2000')).toBeTruthy();
+        });
+
+        it('should copy ctrl methods to scope', function() {
+          var picker   = compileHtml('<div moment-picker="date"></div>'),
+              ctrl     = picker.controller('momentPicker'),
+              isoScope = picker.isolateScope();
+
+          expect(isoScope.setPickerVisibility).toBeTruthy();
+          expect(isoScope.setDisplayMoment).toBeTruthy();
+          expect(isoScope.setPickedMoment).toBeTruthy();
+        });
+
+    });
+
+
+    
+    describe('controller method', function() {
+
+      it('setVisibility should set/unset "ng-hide" class', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker');
+
+        ctrl.setVisibility(false);
+        expect(ctrl.hidden).toBeTruthy();
+        expect(ctrl.visible).toBeFalsy();
+        expect(picker.hasClass('ng-hide')).toBeTruthy();
+
+        ctrl.setVisibility(true);
+        expect(ctrl.hidden).toBeFalsy();
+        expect(ctrl.visible).toBeTruthy();
+        expect(picker.hasClass('ng-hide')).toBeFalsy();
+
+      });
+
+      it('setVisibility should be referenced on the scope', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker'),
+            isoScope = picker.isolateScope();
+
+        expect(ctrl.setVisibility.toString()).toEqual(isoScope.setPickerVisibility.toString());
+
+      });
+
+
+
+      it('setDisplayMoment should set displayMoment from string', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker');
+
+        ctrl.setDisplayMoment('01/01/2000');
+        expect(ctrl.displayMoment.isSame('01/01/2000')).toBeTruthy();
+      });
+
+      it('setDisplayMoment should set displayMoment from moment object', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker');
+
+        ctrl.setDisplayMoment($moment('01/01/2000').add(1, 'year'));
+        expect(ctrl.displayMoment.isSame('01/01/2001')).toBeTruthy();
+      });
+
+      it('setDisplayMoment should set displayMoment on the scope', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker'),
+            isoScope = picker.isolateScope();
+
+        ctrl.setDisplayMoment('01/01/2000');
+        expect(isoScope.displayMoment.isSame('01/01/2000')).toBeTruthy();
+      });
+
+      it('setDisplayMoment should unset to current date', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker');
+
+        ctrl.setDisplayMoment('01/01/2000');
+        ctrl.setDisplayMoment();
+        expect(ctrl.displayMoment.isSame($moment(), 'minute')).toBeTruthy();
+      });
+
+
+
+      it('setPickedMoment should set pickedMoment from string', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker');
+
+        ctrl.setPickedMoment('01/01/2000');
+        expect(ctrl.pickedMoment.isSame('01/01/2000')).toBeTruthy();
+      });
+
+      it('setPickedMoment should set pickedMoment from moment object', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker');
+
+        ctrl.setPickedMoment($moment('01/01/2000').add(1, 'year'));
+        expect(ctrl.pickedMoment.isSame('01/01/2001')).toBeTruthy();
+      });
+
+      it('setPickedMoment should set pickedMoment on the scope', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker'),
+            isoScope = picker.isolateScope();
+
+        ctrl.setPickedMoment('01/01/2000');
+        expect(isoScope.pickedMoment.isSame('01/01/2000')).toBeTruthy();
+      });
+
+      it('setPickedMoment should set displayMoment to match it', function() {
+        var picker = compileHtml('<div moment-picker="date"></div>'),
+            ctrl   = picker.controller('momentPicker');
+
+        ctrl.setPickedMoment('01/01/2000');
+        expect(ctrl.pickedMoment.isSame(ctrl.displayMoment)).toBeTruthy();
+      });
 
     });
 
